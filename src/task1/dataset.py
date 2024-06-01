803f7933955353478f753ef12d19ebcec910f8ce
UNIkeEN <2641712741@qq.com>
2024-06-01 20:55:37 +0800

    feat(dataset): add data loader

--- src/task1/dataset.py
+++ src/task1/dataset.py
@@ -1,9 +1,13 @@
 import pickle
 import os
 from tqdm import tqdm
+import torch
+from torch.utils.data import random_split
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
 from utils import obtain_aig, convert_aig_to_tensor
 
-def process_data(raw_dir: str, save_path: str):
+def generate_data(raw_dir: str, save_path: str):
     """
     Generate dataset for task 1.
 
@@ -34,10 +38,8 @@ def process_data(raw_dir: str, save_path: str):
                         
                         # generate features from aig
                         data = convert_aig_to_tensor(aig_path)
-
                         data['input'] = input
                         data['target'] = target
-
                         all_data.append(data)
                         processed_inputs.add(input)
 
@@ -51,5 +53,52 @@ def process_data(raw_dir: str, save_path: str):
     with open(save_path, 'wb') as f:
         pickle.dump(all_data, f)
 
+
+def load_data(pkl_path, test_ratio=0.2, batch_size=32):
+    """
+    Load data from a pkl file and split into training and testing sets.
+
+    Params:
+    pkl_path (str): path to the pkl file containing the data.
+    test_ratio (float): proportion of the data to include in the test split.
+    batch_size (int): batch size for DataLoader.
+
+    Returns:
+    train_loader (DataLoader): DataLoader for the training set.
+    test_loader (DataLoader): DataLoader for the test set.
+    """
+
+    with open(pkl_path, 'rb') as f:
+        data_list = pickle.load(f)
+
+    # convert data_list to PyTorch Geometric Data objects
+    data_objects = []
+    for item in data_list:
+        edge_index = item['edge_index']
+        node_type = item['node_type']
+        num_inverted_predecessors = item['num_inverted_predecessors']
+        target = item['target']
+
+        x = torch.stack([node_type, num_inverted_predecessors], dim=1).float()
+        y = torch.tensor([target], dtype=torch.float)
+        
+        data = Data(x=x, edge_index=edge_index, y=y)
+        data.num_nodes = item['num_nodes']
+        data.num_edges = item['num_edges']
+        data_objects.append(data)
+
+    num_samples = len(data_objects)
+    num_test = int(test_ratio * num_samples)
+    num_train = num_samples - num_test
+
+    # split the dataset
+    train_data, test_data = random_split(data_objects, [num_train, num_test])
+
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
+
 if __name__ == '__main__':
-    process_data('../../dataset/task1/', '../../dataset/task1_processed.pkl')
+    generate_data('../../dataset/task1/', '../../dataset/task1_small.pkl')
