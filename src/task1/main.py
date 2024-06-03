fff1a1aa9b32b2b11e23d382a5a5173d41865ea1
UNIkeEN <2641712741@qq.com>
2024-06-04 05:35:46 +0800

    feat(main): add loading from checkpoint and drawing loss plot

--- src/task1/main.py
+++ src/task1/main.py
@@ -3,7 +3,7 @@ from omegaconf import OmegaConf
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from tqdm import tqdm
+import matplotlib.pyplot as plt
 from model import GNN, train, evaluate
 from dataset import generate_data, load_data
 
@@ -36,17 +36,37 @@ if __name__ == '__main__':
 
     # define model, loss and optimizer
     model = GNN(cfg.model.hidden_dim).to(device)
+    if cfg.train.load_from_checkpoint == True:
+        model.load_state_dict(torch.load(cfg.train.load_checkpoint_path))
+    model.train()
     optimizer = optim.Adam(model.parameters(), lr=cfg.train.lr)
     criterion = nn.MSELoss()
 
     # train
-    best_test_loss = float('inf')
     print('Begin Training...')
+    best_test_loss = float('inf')
+    train_losses = []
+    test_losses = []
     for epoch in range(cfg.train.epochs):
         train_loss = train(model, train_loader, optimizer, criterion, device)
         test_loss = evaluate(model, test_loader, criterion, device)
+
         print(f'Epoch {epoch+1}/{cfg.train.epochs}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}')
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
 
         if test_loss < best_test_loss:
             best_test_loss = test_loss
-            torch.save(model.state_dict(), cfg.train.checkpoint_path)
+            torch.save(model.state_dict(), cfg.train.save_checkpoint_path)
+
+    # draw loss plot
+    epochs = range(1, cfg.train.epochs + 1)
+    plt.figure(figsize=(10, 6))
+    plt.plot(epochs, train_losses, label='Train Loss')
+    plt.plot(epochs, test_losses, label='Test Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Training and Test Loss Over Epochs')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('./loss-epoch.png')
